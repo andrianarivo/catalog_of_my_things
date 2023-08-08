9be2fdeb14dea0d18daf1bb208d8e253028b9b01
vangelif <ev.fotiades@yahoo.com>
2023-08-08 20:06:53 +0300

    refactor the app and main to plugin with music album and genre classes

--- app.rb
+++ app.rb
@@ -1,18 +1,32 @@
 require_relative 'modules/game_module'
+require_relative 'modules/music_album_module'
+require_relative 'modules/genre_module'
 
 class App
   include GameModule
+  include MusicAlbumModule
+  include GenreModule
 
   def initialize
     @games = []
+    @music_albums = []
+    @genres = []
   end
 
   def run(option)
     case option
+    when 2
+      list_music_album
     when 3
       list_games
-    when 6
+    when 4
+      list_all_genres
+    when 8
+      create_music_album
+    when 9
       create_game
+    when 10
+      create_genre
     else
       puts 'Thank you for using this app!'
       exit
--- takeinput.rb
+++ takeinput.rb
@@ -2,7 +2,7 @@ class TakeInput
   def take_input
     print 'Enter a number: '
     option = gets.chomp.to_i
-    while option.nil? || option < 1 || option > 7
+    while option.nil? || option < 1 || option > 13
       puts 'Please enter a number between 1 and 7!'
       print 'Enter a number: '
       option = gets.chomp.to_i
