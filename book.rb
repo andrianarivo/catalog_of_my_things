8d5c5aa40fe61d4cae6b9ead759ce5f9815251e8
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-08 15:57:40 +0300

    build Book class

---
+++ book.rb
@@ -0,0 +1,14 @@
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state != "bad"
+  end
+end
