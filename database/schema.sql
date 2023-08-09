2b8eacd02f5418f3f87dc63c560cf7fc6c165b6c
Bifabig <biftubig@gmail.com>
2023-08-09 15:11:12 +0300

    add schema to create games and author tables

--- database/schema.sql
+++ database/schema.sql
@@ -59,3 +59,17 @@ CREATE TABLE label(
     title VARCHAR(255),
     color VARCHAR(255),
 );
+
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  PRIMARY KEY(id)
+);
