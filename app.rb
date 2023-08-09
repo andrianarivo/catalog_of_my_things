b4e46fee0046ae5b97f1957fa747a7c9c6583e6b
Bifabig <biftubig@gmail.com>
2023-08-09 15:07:53 +0300

    include author module and modify run method

--- app.rb
+++ app.rb
@@ -1,19 +1,22 @@
 require_relative 'modules/game_module'
 require_relative 'modules/music_album_module'
 require_relative 'modules/genre_module'
+require_relative 'modules/author_module'
 
 class App
   include GameModule
   include MusicAlbumModule
   include GenreModule
+  include AuthorModule
 
   def initialize
     @games = []
     @music_albums = []
     @genres = []
+    @authors = []
   end
 
-  def run(option)
+  def first_run(option)
     case option
     when 2
       list_music_album
@@ -21,12 +24,23 @@ class App
       list_games
     when 4
       list_all_genres
+    when 6
+      list_authors
+    else
+      second_run(option)
+    end
+  end
+
+  def second_run(option)
+    case option
     when 8
       create_music_album
     when 9
       create_game
     when 10
       create_genre
+    when 12
+      create_author
     else
       puts 'Thank you for using this app!'
       exit
